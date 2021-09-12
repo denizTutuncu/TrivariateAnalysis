615c637adb77f24c1c978c28ae508005be8b1f5d
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-09-11 17:25:14 -0700

    Add TrivariateAnalysis Project

---
+++ TrivariateAnalysis/TrivariateAnalysis/TrivariateAnalysis.h
@@ -0,0 +1,18 @@
+//
+//  TrivariateAnalysis.h
+//  TrivariateAnalysis
+//
+//  Created by Deniz Tutuncu on 9/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrivariateAnalysis.
+FOUNDATION_EXPORT double TrivariateAnalysisVersionNumber;
+
+//! Project version string for TrivariateAnalysis.
+FOUNDATION_EXPORT const unsigned char TrivariateAnalysisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrivariateAnalysis/PublicHeader.h>
+
+
